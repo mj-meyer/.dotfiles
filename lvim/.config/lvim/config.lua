00d3b9ee75687e44cc6b0549247b4728ca568c6e
mj-meyer <hello@mjmeyer.dev>
2024-02-20 11:37:33 +1300

    (lvim) change nvim tree to have floating file tree

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -14,6 +14,32 @@ lvim.format_on_save = true
 lvim.colorscheme = "tokyonight"
 lvim.transparent_window = true
 lvim.builtin.nvimtree.setup.view.float.enable = true
+
+-- Float Nvimtree in middle of the screen
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.8  -- You can change this too
+lvim.builtin.nvimtree.setup.view.float.open_win_config = function()
+  local screen_w = vim.opt.columns:get()
+  local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+  local window_w = screen_w * WIDTH_RATIO
+  local window_h = screen_h * HEIGHT_RATIO
+  local window_w_int = math.floor(window_w)
+  local window_h_int = math.floor(window_h)
+  local center_x = (screen_w - window_w) / 2
+  local center_y = ((vim.opt.lines:get() - window_h) / 2)
+      - vim.opt.cmdheight:get()
+  return {
+    border = 'rounded',
+    relative = 'editor',
+    row = center_y,
+    col = center_x,
+    width = window_w_int,
+    height = window_h_int,
+  }
+end
+lvim.builtin.nvimtree.setup.view.width = function()
+  return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+end
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
