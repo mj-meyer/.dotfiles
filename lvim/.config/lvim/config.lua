6e57bd7f99f6bc4836da21439a241498c98a9718
mj-meyer <hello@mjmeyer.dev>
2024-08-02 15:52:56 +1200

    (lvim) add refactoring capability to update all files when vars change

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -421,6 +421,16 @@ lvim.plugins = {
     ft = { "fugitive" }
   },
   { "shumphrey/fugitive-gitlab.vim" }
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
